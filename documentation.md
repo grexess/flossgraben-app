ac8e33ab5a099d483d45fda43c4dac45f5385eef
Gregor Schumann <gregor.schumann@sap.com>
2017-10-26 17:04:35 +0200

    26.10.2017
    26.10.2017

---
+++ documentation.md
@@ -0,0 +1,1 @@
+# Flossgraben-App Infosheet
--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,105 +1,92 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
-import { Tasks } from '../api/tasks.js';
+import { ReactiveDict } from 'meteor/reactive-dict';
+
+import { Runners } from '../api/runners.js';
+
+import './runner.js';
+import './birthSelection.js';
 import './body.html';
 
-Template.body.helpers({
- tasks() {
-	 return Tasks.find({});
- },
+Template.body.onCreated(function bodyOnCreated() {
 });
 
-$(document).ready(function () {
+Template.body.events({
 
-	registerTopLevelClick();
-	//registerSecondLevelClick();
-	countDown();
-	$(".content").html($("#Home-Content").html());
-	//set Home to active node
-	var home = $('.click')[0];
-	$(home).addClass("active1");
-});
+	//submit a new runner
+	'click #submitBtn': function (event, instance) {
+		event.preventDefault();
 
-function registerTopLevelClick() {
+		const birthday = instance.$('#dob-day :selected').text() + "." + instance.$('#dob-year :selected').text() + "." + instance.$('#dob-year :selected').text();
 
-	$('.click').click(function () {
-		$('.click').removeClass("active1");
-		$(this).addClass("active1");
-		// replace the contents of the div with the link text
-		var secondNavElem = $("#" + $(this).text() + "-SecondNav");
-		var subNavElements = secondNavElem.find("[data-target]");
-		if (subNavElements.length > 0) {
-			var subContent = subNavElements[0].dataset.target;
-			$(".content").html($("#" + subContent).html());
-			$(subNavElements[0]).parent().addClass("active1")
-			$(".secondNav").html(secondNavElem.html());
-			registerSecondLevelClick();
-		} else {
-			$(".content").html($("#" + $(this).text() + "-Content").html());
-			$(".secondNav").html("");
-		};
-		// cancel the default action of the link by returning false
-		return false;
-	});
-}
-
-function registerSecondLevelClick() {
-	
-	debugger;
-
-	$('.clickSecNav').click(function () {
-
-		$(".clickSecNav").removeClass("secondNavListActive").addClass("secondNavListInactive"); ;
-		$(this).removeClass("secondNavListInactive").addClass("secondNavListActive");
-		$(".content").html($("#" + this.dataset.target).html());
-
-		//exception for pciture right
-		if (this.dataset.target == "2018-Strecke") {
-			$(".right").html('<img src="https://my.flossgraben.run/images/runner.png" />')
-		} else {
-			$(".right").html("");
-		}
-		// cancel the default action of the link by returning false
-		return false;
-	});
-}
-
-function myFunction() {
-
-	var x = document.getElementById("myTopnav");
-	if (x.className === "topnav") {
-		x.className += " responsive";
-	} else {
-		x.className = "topnav";
+		Runners.insert({
+			firstName: instance.$('#firstName').val(),
+			lastName: instance.$('#lastName').val(),
+			club: instance.$('#club').val(),
+			gender: instance.$('input[name="gender"]:checked').val(),
+			birthday: birthday
+		});
 	}
+});
+
+Template.thisYear.onCreated(function bodyOnCreated() {
+	Meteor.subscribe('runners');
+});
+
+Template.body.helpers({
+	runners() {
+		return Runners.find({});
+
+},
+runnersCount: function () {
+	return Runners.find().count()
 }
+});
 
-function countDown() {
-	// Set the date we're counting down to
-	var startDate = "Jan 5, 2018 15:37:25";
-	var countDownDate = new Date(startDate).getTime();
+Template.thisYear.helpers({
+	runners() {
+			return Runners.find({});
 
-	// Update the count down every 1 second
-	var x = setInterval(function () {
+	},
+	runnersCount: function () {
+		return Runners.find().count()
+	}
+});
 
-			// Get todays date and time
-			var now = new Date().getTime();
+Template.registerform.events({
 
-			// Find the distance between now an the count down date
-			var distance = countDownDate - now;
+	'submit .addRunnerForm': function (event) {
+		// Prevent default browser form submit
+		event.preventDefault();
 
-			// Time calculations for days, hours, minutes and seconds
-			var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-			var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-			var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-			var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+		// Get value from form element
+		//var name = event.target.textbox1.value;
 
-			// Display the result in the element with id="2018"
-			document.getElementById("countdown").innerHTML = "Noch <b>" + days + "  </b>Tage <b>" + hours + "</b> Stunden <b>" + minutes + "</b> Minuten <b>" + seconds + "</b> Sekunden bis zum Start am  " + startDate + "!";
+		alert('name');
+	},
 
-			// If the count down is finished, write some text
-			if (distance < 0) {
-				clearInterval(x);
-				document.getElementById("countdown").innerHTML = "EXPIRED";
-			}
-		}, 1000);
-}
+	'click': function (event, templ) {
+
+		event.preventDefault();
+		alert('Click');
+	}
+	/*
+
+	'submit #addRunnerForm'(event) {
+		// Prevent default browser form submit
+		event.preventDefault();
+		debugger;
+		// Get value from form element
+		const target = event.target;
+		const text = target.text.value;
+		/*
+		   // Insert a task into the collection
+		   Tasks.insert({
+			 text,
+			 createdAt: new Date(), // current time
+		   });
+	    
+		   // Clear form
+		   target.text.value = ''; */
+
+});
